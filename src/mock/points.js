a713cd69f0d973777bf426bd0ce5188526a09f2f
Ekaterina Nesterenko <89526050507@mail.ru>
2025-03-18 17:26:03 +0300

    Исправление моковых данных

--- src/mock/points.js
+++ src/mock/points.js
@@ -6,7 +6,7 @@ const mockPoints = [
     basePrice: 1100,
     dateFrom: '2025-01-10T12:00',
     dateTo: '2025-01-10T13:00',
-    destination: '111',
+    destination: 111,
     isFavorite: true,
     offers: [],
     type: 'taxi'
@@ -14,10 +14,10 @@ const mockPoints = [
 
   {
     id: 2,
-    basePrice: 1100,
+    basePrice: 1200,
     dateFrom: '2025-01-10T14:00',
     dateTo: '2025-01-10T20:00',
-    destination: '222',
+    destination: 222,
     isFavorite: false,
     offers: [21, 22, 23],
     type: 'flight'
@@ -25,10 +25,10 @@ const mockPoints = [
 
   {
     id: 3,
-    basePrice: 1100,
+    basePrice: 1300,
     dateFrom: '2025-01-10T22:00',
     dateTo: '2025-01-11T12:00',
-    destination: '333',
+    destination: 333,
     isFavorite: true,
     offers: [31, 32],
     type: 'bus'
@@ -36,10 +36,10 @@ const mockPoints = [
 
   {
     id: 4,
-    basePrice: 1100,
+    basePrice: 1400,
     dateFrom: '2025-01-11T20:00',
     dateTo: '2025-01-12T15:00',
-    destination: '444',
+    destination: 444,
     isFavorite: false,
     offers: [41, 42],
     type: 'train'
@@ -50,13 +50,13 @@ const mockPoints = [
     basePrice: 1100,
     dateFrom: '2025-01-12T16:00',
     dateTo: '2025-01-12T17:00',
-    destination: '555',
+    destination: 555,
     isFavorite: true,
     offers: [51, 52, 53],
     type: 'check-in'
   },
 ];
 
-const getRandomMockPoints = getRandomArrayElement(mockPoints);
+const getRandomMockPoints = () => getRandomArrayElement(mockPoints);
 
 export { getRandomMockPoints };
