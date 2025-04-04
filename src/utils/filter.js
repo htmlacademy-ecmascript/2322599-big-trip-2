3848a3ffa19202a4a0522378e4535b4d59ddca7d
Ekaterina Nesterenko <89526050507@mail.ru>
2025-04-04 18:10:14 +0300

    правки: изменение порядка фильтров

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -3,9 +3,9 @@ import { isPointPast, isPointPresent, isPointFuture } from './utils.js';
 
 const filter = {
   [FilterType.EVERYTHING]: (points) => points,
-  [FilterType.PAST]: (points) => points.filter((point) => isPointPast(point.dateFrom)),
-  [FilterType.PRESENT]: (points) => points.filter((point) => isPointPresent(point.dateFrom)),
   [FilterType.FUTURE]: (points) => points.filter((point) => isPointFuture(point.dateFrom)),
+  [FilterType.PRESENT]: (points) => points.filter((point) => isPointPresent(point.dateFrom)),
+  [FilterType.PAST]: (points) => points.filter((point) => isPointPast(point.dateFrom)),
 };
 
 export { filter };
