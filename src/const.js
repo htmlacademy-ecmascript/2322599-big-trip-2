9b88bf27f9c5f53e7a14a00662e4abbcb9343da8
Ekaterina Nesterenko <89526050507@mail.ru>
2025-05-02 19:42:07 +0300

    Удаление моков

--- src/const.js
+++ src/const.js
@@ -1,21 +1,3 @@
-const MAX_COUNT = 100;
-
-const DESCRIPTIONS_DESTINATION = [
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  'Cras aliquet varius magna, non porta ligula feugiat eget.',
-  'Fusce tristique felis at fermentum pharetra.',
-  'Aliquam id orci ut lectus varius viverra.',
-  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
-  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
-  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
-  'Sed sed nisi sed augue convallis suscipit in sed felis.',
-  'Aliquam erat volutpat.',
-  'Nunc fermentum tortor ac porta dapibus.',
-  'In rutrum ac purus sit amet tempus.'
-];
-
-const NAME_DESTINATION = ['Moscow', 'Rostov-on-Don', 'Irkutsk', 'Sochi', 'Krasnodar', 'Kaliningrad'];
-
 const EVENT_TYPES = [
   { type: 'taxi', label: 'Taxi' },
   { type: 'bus', label: 'Bus' },
@@ -56,4 +38,4 @@ const UpdateType = {
   INIT: 'INIT',
 };
 
-export { DESCRIPTIONS_DESTINATION, NAME_DESTINATION, MAX_COUNT, EVENT_TYPES, FilterType, SortType, UserAction, UpdateType };
+export { EVENT_TYPES, FilterType, SortType, UserAction, UpdateType };
